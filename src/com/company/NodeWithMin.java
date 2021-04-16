1073871bc09eba7380decd62c693422496108d9c
Joshua Yoon <joshyoon92@gmail.com>
2021-04-16 10:13:39 +0900

    Initial commit

---
+++ src/com/company/NodeWithMin.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class NodeWithMin {
+    public int value;
+    public int min;
+    public NodeWithMin(int v, int min) {
+        value = v;
+        this.min= min;
+    }
+
+}
